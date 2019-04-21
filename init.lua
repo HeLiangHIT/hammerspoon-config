15e3870a23118b8f0c48302dd53ac7631a05ee42
heliang <1042248300@qq.com>
2019-04-21 12:23:56 +0800

    modify my

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 print('==================================================')
-require "headphone.headphone"
+-- require "headphone.headphone"
 require "hotkey.hotkey"
 require "ime.ime"
 require "reload.reload"
 require "usb.usb"
-require "wifi.wifi"
 require "window.window"
 require "clipboard.clipboard"
 require "statuslets.statuslets"
@@ -13,6 +12,6 @@ require "weather.weather"
 require "speaker.speaker"
 
 -- Private use
-if (hs.host.localizedName() == 'kaboom的MacBook Pro') then
-	require("autoscript.autoscript")
-end
+-- if (hs.host.localizedName() == 'kaboom的MacBook Pro') then
+-- 	require("autoscript.autoscript")
+-- end
